beb9c2201f1ea4b262ec7abea52791da3c825527
rizend <lancerroy@gmail.com>
2014-10-14 14:55:23 -0400

    Corrected domainLink shell script

--- domainLink.sh
+++ domainLink.sh
@@ -1,2 +1,2 @@
 xhost +local:root
-gksu sh -c 'echo -e "yes\nyes\n" | bash $(ls | grep pbis); zenity --error --text="Remember to change the line \'session sufficient    pam_lsass.so\' and change \'sufficient\' to \'[success=ok default=ignore]\' Ready?";gedit /etc/pam.d/common-session'
+gksu sh -c 'echo -e "yes\nyes\n" | bash $(ls | grep pbis); cp /RV/inst/common-session /etc/pam.d/'
