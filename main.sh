b77ba5f3a7b1d5d999d00ef36e9631aac8bdd7fb
rizend <lancerroy@gmail.com>
2014-10-21 21:36:29 -0400

    made domain link executable

--- main.sh
+++ main.sh
@@ -9,11 +9,11 @@ echo $MACADDR > /RV/inst/mac.txt
 cd /RV/inst/APSinstall/
 wget http://download.beyondtrust.com/PBISO/8.2/linux.deb.i386/pbis-open-8.2.0.2969.linux.x86.deb.sh
 chmod +x $(ls | grep pbis)
-chmod +x /RV/inst/APSlinux/domainLink.sh
 chmod 755 /RV/inst/APSlinux/d2.sh
 mkdir -p /home/bak/Desktop/
 cp -r /etc/skel/ /home/bak
 cp /RV/inst/APSinstall/domainLink.sh /home/bak/Desktop/
+chmod +x /home/bak/Desktop/domainLink.sh
 chown -R bak:bak /home/bak
 
 printf "[SeatDefaults]\nallow-guest=false\n" > /usr/share/lightdm/lightdm.conf.d/50-no-guest.conf
